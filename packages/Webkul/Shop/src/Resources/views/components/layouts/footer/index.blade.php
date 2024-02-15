dec9278a657d15d56152ff80572ef814e378d640
vikas vishwakarma <vikas.vishwakarma749@webkul.com>
2024-02-15 14:06:11 +0530

    footer re-designed

--- packages/Webkul/Shop/src/Resources/views/components/layouts/footer/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/footer/index.blade.php
@@ -19,68 +19,107 @@
 
 <footer class="mt-[36px] bg-white max-sm:mt-[30px] border-t-4 border-[#d5cfcf]">
     @if ($customization)
-        <div class="flex gap-x-[25px] gap-y-[30px] p-[60px] max-1060:flex-wrap max-1060:flex-col-reverse max-sm:px-[15px]">
-            <a
-                href="{{ route('shop.home.index') }}"
-                class="place-self-start -mt-[4px]"
-                aria-label="Bagisto "
-            >
-                <img
-                    src="{{ core()->getCurrentChannel()->logo_url ?? bagisto_asset('images/logo.svg') }}"
-                    width="400"
-                    height="200"
-                    alt="Bagisto"
+        <div class="flex gap-20 p-10">
+            <div class="w-[370px]">
+                <a
+                    href="{{ route('shop.home.index') }}"
+                    class="place-self-start -mt-[4px]"
+                    aria-label="Bagisto "
                 >
-            </a>
+                    <img
+                        src="{{ core()->getCurrentChannel()->logo_url ?? bagisto_asset('images/logo.svg') }}"
+                        width="400"
+                        height="200"
+                        alt="Bagisto"
+                    >
+                </a>
+                <div class="flex flex-wrap gap-4 mt-[50px]">
+                    <div class="flex gap-3 icon-address text-wrap w-[300px] !font-['Poppins']">
+                        <a href="javascript:void(0)">
+                            @lang('7 ADB Ave, Ortigas Center, pasig, Metro Manila')
+                        </a>
+                    </div>
 
-            <div class="flex gap-[15px] items-start px-5 flex-wrap max-1180:gap-[25px] max-1060:justify-between border-l-2">
-                <h1 class="font-bold">
-                    @lang('Reamulan Lands Inc.')
-                </h1>
+                    <div class="flex gap-3 icon-email-footer !font-['Poppins']">
+                        <a href="mailto:@lang('sample@gmail.com')">
+                            @lang('sample@gmail.com')
+                        </a>
+                    </div>
 
-                <ul class="grid gap-[15px] text-[14px]">
-                    <li>@lang('7 ADB Ave, Ortigas Center, pasig, Metro Manila')</li>
-                    <li>@lang('sample@gmail.com')</li>
-                    <li>@lang('+63 9456677654')</li>
-                </ul>
+                    <div class="flex gap-3 icon-phone !font-['Poppins']">
+                        <a href="tel:@lang('+63 9456677654')">
+                            @lang('+63 9456677654')
+                        </a>
+                    </div>
+                </div>
             </div>
 
-            <div class="flex gap-[15px] items-start px-5 flex-wrap max-1180:gap-[25px] max-1060:justify-between border-l-2">
-                <h1 class="font-bold">
-                    @lang('Quicklinks')
-                </h1>
+            <div class="pl-20 py-0 border-l-2 border-[#c9e1ed]">
+                @if ($customization->options)
+                    @foreach ($customization->options as $footerLinkSection)
+                        <ul class="grid gap-[15px] text-[14px]">
+                            @php
+                                usort($footerLinkSection, function ($a, $b) {
+                                    return $a['sort_order'] - $b['sort_order'];
+                                });
+                            @endphp
 
-                <div class="flex gap-[15px] items-start flex-wrap max-1180:gap-[25px] max-1060:justify-between">
-                    @if ($customization->options)
-                        @foreach ($customization->options as $footerLinkSection)
-                            <ul class="grid gap-[15px] text-[14px]">
-                                @php
-                                    usort($footerLinkSection, function ($a, $b) {
-                                        return $a['sort_order'] - $b['sort_order'];
-                                    });
-                                @endphp
-                                
-                                @foreach ($footerLinkSection as $link)
-                                    <li>
-                                        <a href="{{ $link['url'] }}">
-                                            {{ $link['title'] }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>    
-                        @endforeach
-                    @endif
+                            <li class="text-[30px] flex font-bold">
+                                @lang('Quicklinks')
+                            </li>
+
+                            @foreach ($footerLinkSection as $link)
+                                <li>
+                                    <a href="{{ $link['url'] }}">
+                                        {{ $link['title'] }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>    
+                    @endforeach
+                @endif
+            </div>
+
+            <div class="pl-20 border-l-2 border-[#c9e1ed]">
+                <div class="flex gap-4">
+                    <h1 class="text-[30px] flex font-bold">
+                        @lang('Follow Us')
+                    </h1>
+                    
+                    <!-- Facebook -->
+                    <div class="p-[10px] !bg-gradient-to-r from-[#e0165d] to-yellow-500 rounded-full">
+                        <a href="#" alt="facebook">
+                            <img src="{{ bagisto_asset('images/facebook.svg') }}" alt="facebook">   
+                        </a>
+                    </div>
+
+                    <!-- Instagram -->
+                    <div class="p-[10px] !bg-gradient-to-r from-[#e0165d] to-yellow-500 rounded-full">
+                        <a href="#" alt="instagram">
+                            <img src="{{ bagisto_asset('images/instagram.png') }}" alt="instagram">   
+                        </a>
+                    </div>
+
+                    <!-- Youtube -->
+                    <div class="p-[10px] !bg-gradient-to-r from-[#e0165d] to-yellow-500 rounded-full">
+                        <a href="#" alt="youtube">
+                            <img src="{{ bagisto_asset('images/youtube.png') }}" alt="youtube" class="my-[4px]">   
+                        </a>
+                    </div>
+
+                    <!-- tiktok -->
+                    <div class="p-[10px] !bg-gradient-to-r from-[#e0165d] to-yellow-500 rounded-full">
+                        <a href="#" alt="tiktok">
+                            <img src="{{ bagisto_asset('images/tiktok.png') }}" alt="tiktok">   
+                        </a>
+                    </div>
                 </div>
-                
-                {{-- News Letter subscription --}}
+
+                <!-- News Letter subscription -->
                 @if (core()->getConfigData('customer.settings.newsletter.subscription'))
                     <div class="grid gap-[10px]">
-                        <p class="max-w-[288px] leading-[45px] text-[30px] italic text-navyBlue">
+                        <p class="max-w-[288px] leading-[45px] text-[20px]">
                             @lang('shop::app.components.layouts.footer.newsletter-text')
-                        </p>
-
-                        <p class="text-[12px]">
-                            @lang('shop::app.components.layouts.footer.subscribe-stay-touch')
                         </p>
 
                         <x-shop::form
@@ -94,10 +133,10 @@
                             <x-shop::form.control-group.control
                                 type="email"
                                 name="email"
-                                class=" blockw-[420px] max-w-full px-[20px] py-[20px] pr-[110px] bg-[#F1EADF] border-[2px] border-[#E9DECC] rounded-[12px] text-xs font-medium max-1060:w-full"
+                                class="block w-[420px] h-[50px] max-w-full px-[20px] py-[20px] pr-[110px] bg-[#e5e5e5] text-xs font-medium max-1060:w-full"
                                 rules="required|email"
                                 label="Email"
-                                placeholder="email@example.com"
+                                placeholder="Email Address"
                             >
                             </x-shop::form.control-group.control>
 
@@ -108,21 +147,18 @@
 
                                 <button
                                     type="submit"
-                                    class=" absolute flex items-center top-[8px] w-max px-[26px] py-[13px] bg-white rounded-[12px] text-[12px] font-medium rtl:left-[8px] ltr:right-[8px]"
+                                    class="absolute flex items-center h-[50px] !w-[110px] top-[0px] w-max px-[38px] py-[13px] bg-white text-[12px] text-white font-medium rtl:left-[0px] ltr:right-[0px] !bg-gradient-to-r from-[#e0165d] to-yellow-500"
                                 >
-                                    @lang('shop::app.components.layouts.footer.subscribe')
+                                    @lang('shop::app.components.layouts.footer.send')
                                 </button>
                             </div>
                         </x-shop::form>
+
+                        <p class="text-[14px] mt-[50px]">
+                            @lang('shop::app.components.layouts.footer.subscribe-stay-touch')
+                        </p>
                     </div>
                 @endif
-            </div>
-
-            <div class="flex gap-[15px] items-start px-5 flex-wrap max-1180:gap-[25px] max-1060:justify-between border-l-2">
-                <h1 class="font-bold">
-                    @lang('Socials')
-                </h1>
-
                 
             </div>
         </div>
