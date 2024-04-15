e7de4dd3848be23f162cdff4e3d04957842502b0
vikas vishwakarma <vikas.vishwakarma749@webkul.com>
2024-04-15 17:55:02 +0530

    remove modal for ifream

--- packages/Webkul/Enclaves/src/Resources/views/shop/checkout/ekyc/index.blade.php
+++ packages/Webkul/Enclaves/src/Resources/views/shop/checkout/ekyc/index.blade.php
@@ -15,10 +15,10 @@
     <script type="text/x-template" id="v-user-kyc-summary-template">
         <!-- Page Content -->
         <div class="container">
-            <div class="p-[20px] m-[20px]">
-                <div class="w-full flex justify-center gap-x-[40px] max-[1180px]:gap-x-[20px]">
+            <div class="m-[20px] p-[20px]" v-if="! sended && ! embedURL">
+                <div class="flex w-full justify-center gap-x-[40px] max-[1180px]:gap-x-[20px]">
                     <h1 
-                        class="text-[40px] font-bold mt-[26px] leading-[48px] max-sm:text-[26px] max-sm:leading-[36px]"
+                        class="mt-[26px] text-[40px] font-bold leading-[48px] max-sm:text-[26px] max-sm:leading-[36px]"
                         :class="opacity"
                     >
                         @lang('enclaves::app.shop.authentication.title')
@@ -26,7 +26,7 @@
                 </div>
            
                 <p 
-                    class="text-[20px] mt-[50px] max-sm:text-[16px] max-sm:mt-[25px]" 
+                    class="mt-[50px] text-[20px] max-sm:mt-[25px] max-sm:text-[16px]" 
                     :class="opacity"
                 >
                     @lang('enclaves::app.shop.authentication.body_text')
@@ -36,11 +36,11 @@
                 <div 
                     v-if="sended"
                     role="status" 
-                    class="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
+                    class="absolute left-1/2 top-2/4 -translate-x-1/2 -translate-y-1/2"
                     >
                     <svg 
                         aria-hidden="true" 
-                        class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" 
+                        class="h-8 w-8 animate-spin fill-gray-600 text-gray-200 dark:fill-gray-300 dark:text-gray-600" 
                         viewBox="0 0 100 101" 
                         fill="none" 
                         xmlns="http://www.w3.org/2000/svg"
@@ -55,12 +55,12 @@
                         />
                     </svg>
 
-                    <span class="absolute text-nowrap -left-[80px]" v-text="loadingText"></span>
+                    <span class="absolute -left-[80px] text-nowrap" v-text="loadingText"></span>
                 </div>
                 
                 <button
                     v-if="sended"
-                    class="flex mt-[30px] block mx-auto bg-[linear-gradient(268.1deg,_#CC035C_7.47%,_#FCB115_98.92%)] text-white text-[16px] font-medium py-[16px] px-[43px] rounded-[18px] text-center"
+                    class="mx-auto mt-[30px] flex rounded-[18px] bg-[linear-gradient(268.1deg,_#CC035C_7.47%,_#FCB115_98.92%)] px-[43px] py-[16px] text-center text-[16px] font-medium text-white"
                     :class="opacity"
                 >
                     @lang('enclaves::app.shop.authentication.authenticate')
@@ -68,31 +68,46 @@
 
                 <button
                     v-else
-                    class="flex mt-[30px] block mx-auto bg-[linear-gradient(268.1deg,_#CC035C_7.47%,_#FCB115_98.92%)] text-white text-[16px] font-medium py-[16px] px-[43px] rounded-[18px] text-center"
+                    class="mx-auto mt-[30px] flex rounded-[18px] bg-[linear-gradient(268.1deg,_#CC035C_7.47%,_#FCB115_98.92%)] px-[43px] py-[16px] text-center text-[16px] font-medium text-white"
                     @click="handleKycVerification"
                 >
                     @lang('enclaves::app.shop.authentication.authenticate')
                 </button>
-                
-                <x-admin::modal ref="addFreamModal">
-                    <x-slot:header>
-                        <p class="text-[18px] text-gray-800 dark:text-white font-bold">
-                            @lang('enclaves::app.shop.authentication.title')
-                        </p>
-                    </x-slot:header>
+            </div>
+            <div v-else>
 
-                    <x-slot:content>
-                        <div class="p-5">
-                            <iframe 
-                                :src="embedURL"
-                                width="560" 
-                                height="330" 
-                                title=""
-                                >
-                            </iframe>
-                        </div>
-                    </x-slot:content>
-                </x-admin::modal>
+                <div 
+                    v-if="! embedURL"
+                    role="status" 
+                    class="absolute left-1/2 top-2/4 -translate-x-1/2 -translate-y-1/2"
+                    >
+                    <svg 
+                        aria-hidden="true" 
+                        class="h-8 w-8 animate-spin fill-gray-600 text-gray-200 dark:fill-gray-300 dark:text-gray-600" 
+                        viewBox="0 0 100 101" 
+                        fill="none" 
+                        xmlns="http://www.w3.org/2000/svg"
+                        >
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" 
+                            fill="currentColor"
+                        />
+
+                        <path 
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"
+                        />
+                    </svg>
+
+                    <span class="absolute -left-[80px] text-nowrap" v-text="loadingText"></span>
+                </div>
+
+                <iframe 
+                    :src="embedURL"
+                    width="1160" 
+                    height="700" 
+                    title=""
+                >
+                </iframe>
             </div>
         </div>
     </script>
@@ -116,7 +131,7 @@
             mounted() {
                 if (this.verification) {
                     this.sended = true;
-                    this.opacity = 'opacity-20';
+                    this.opacity = '!opacity-20';
                     this.loadingText = "Please complete Kyc process";
                 }
 
@@ -188,8 +203,6 @@
                     })
                     .then(response => {
                         if (! response.data.data.status && response.data.data.url) {
-                            this.$refs.addFreamModal.open();
-
                             this.embedURL = response.data.data.url;
                         }
                     })
